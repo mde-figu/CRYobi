e8e4c640f120f4148ea8ad803a8cd65900ef79d1
Mirks <markusfwirz@gmail.com>
2025-08-14 00:30:09 -0300

    vibeCodingGensVibeErrors

--- src/CastleBoard.cpp
+++ src/CastleBoard.cpp
@@ -1,43 +1,53 @@
 // CastleBoard Code
 #include <Arduino.h>
 #include <Wire.h>
+#include <SoftwareSerial.h>
 
 const byte CASTLE_ID = 1; // Change this for each CastleBoard: 1 to 4
-const int DE_PIN = 2; // RS485 DE pin
+const byte DE_PIN = 12;   // RS485 Driver Enable
+const byte DI_PIN = 11;   // RS485 Data In (TX)
+const byte RO_PIN = 10;   // RS485 Receive Out (RX)
 const byte START_BYTE = 0xAA;
 const byte END_BYTE = 0xBB;
 const byte MOTOR_BASE_ADDR = 0x08; // MotorBoards from 0x08 to 0x12 (11 boards)
 const byte NUM_MOTOR_BOARDS = 11;
-
-byte pot_values[22]; // 11 boards * 2 channels
 
 void processCommand(byte cmd, byte len, byte* data);
 void fetchAllPots();
 void setMotorTarget(byte motor_index, byte value);
 void sendResponse(byte id, byte cmd, byte len, byte* data);
 
+SoftwareSerial rs485(RO_PIN, DI_PIN); // RX, TX for RS485
+byte pot_values[22]; // 11 boards * 2 channels
+
 void setup() {
-  Serial.begin(9600); // RS485 baud rate
+  Serial.begin(9600); // For terminal output
+  rs485.begin(9600);  // RS485 communication
   pinMode(DE_PIN, OUTPUT);
-  digitalWrite(DE_PIN, LOW); // Receive mode by default
+  digitalWrite(DE_PIN, LOW); // Receive mode
 
   Wire.begin(); // I2C master for MotorBoards
+  Serial.print("CastleBoard ");
+  Serial.print(CASTLE_ID);
+  Serial.println(" started");
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    byte incoming = Serial.read();
+  if (rs485.available() > 0) {
+    byte incoming = rs485.read();
     if (incoming == START_BYTE) {
-      byte id = Serial.read();
+      while (rs485.available() < 3); // Wait for ID, CMD, LEN
+      byte id = rs485.read();
       if (id == CASTLE_ID) {
-        byte cmd = Serial.read();
-        byte len = Serial.read();
-        byte data[22]; // Max len for pots
+        byte cmd = rs485.read();
+        byte len = rs485.read();
+        byte data[22];
+        while (rs485.available() < len + 2); // Wait for DATA + CHECKSUM + END
         for (byte i = 0; i < len; i++) {
-          data[i] = Serial.read();
+          data[i] = rs485.read();
         }
-        byte checksum = Serial.read();
-        byte end = Serial.read();
+        byte checksum = rs485.read();
+        byte end = rs485.read();
 
         byte calc_checksum = id ^ cmd ^ len;
         for (byte i = 0; i < len; i++) {
@@ -46,6 +56,8 @@ void loop() {
 
         if (end == END_BYTE && checksum == calc_checksum) {
           processCommand(cmd, len, data);
+        } else {
+          Serial.println("Castle: Invalid packet");
         }
       }
     }
@@ -74,9 +86,18 @@ void fetchAllPots() {
       byte ch2 = Wire.read();
       byte p2 = Wire.read();
       if (ch1 == 1 && ch2 == 2) {
-        pot_values[board * 2] = p1;
-        pot_values[board * 2 + 1] = p2;
+        // Scale 0-255 to 0-99 for display
+        pot_values[board * 2] = map(p1, 0, 255, 0, 99);
+        pot_values[board * 2 + 1] = map(p2, 0, 255, 0, 99);
       }
+      Serial.print("Address: " + addr);
+      Serial.print("Ch1: " + ch1);
+      Serial.print("Pot1: " + p1);
+      Serial.print("Ch2: " + ch2);
+      Serial.print("Pot2: " + p2);
+    } else {
+      Serial.print("Castle: No response from MotorBoard 0x");
+      Serial.println(addr, HEX);
     }
   }
 }
@@ -85,25 +106,31 @@ void setMotorTarget(byte motor_index, byte value) {
   byte board = motor_index / 2;
   byte channel = (motor_index % 2) + 1;
   byte addr = MOTOR_BASE_ADDR + board;
+  // Scale 0-99 to 0-255 for MotorBoard
+  byte scaled_value = map(value, 0, 99, 0, 255);
   Wire.beginTransmission(addr);
   Wire.write(channel);
-  Wire.write(value);
+  Wire.write(scaled_value);
   Wire.endTransmission();
+  Serial.print("Castle: Set Motor ");
+  Serial.print(motor_index);
+  Serial.print(" to ");
+  Serial.println(value);
 }
 
 void sendResponse(byte id, byte cmd, byte len, byte* data) {
   digitalWrite(DE_PIN, HIGH);
-  Serial.write(START_BYTE);
-  Serial.write(id);
-  Serial.write(cmd);
-  Serial.write(len);
+  rs485.write(START_BYTE);
+  rs485.write(id);
+  rs485.write(cmd);
+  rs485.write(len);
   byte checksum = id ^ cmd ^ len;
   for (byte i = 0; i < len; i++) {
-    Serial.write(data[i]);
+    rs485.write(data[i]);
     checksum ^= data[i];
   }
-  Serial.write(checksum);
-  Serial.write(END_BYTE);
-  Serial.flush();
+  rs485.write(checksum);
+  rs485.write(END_BYTE);
+  rs485.flush();
   digitalWrite(DE_PIN, LOW);
 }
